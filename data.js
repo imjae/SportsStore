109e8974f1acfcbffef275ba308f04a195b2ca33
imjae <code@kakao.com>
2021-01-17 17:45:31 +0900

    sportsstore 초기 셋팅 완료

---
+++ data.js
@@ -0,0 +1,17 @@
+module.exports = function() {
+    return {
+        categories: ["Watersports", "Soccer", "Chess"],
+        products: [
+            {id: 1, name: "Kayak", category: "Watersports", descriptsion: "A boat for one person", price: 275},
+            {id: 2, name: "Lifejacker", category: "Watersports", descriptsion: "Protective and fashionable", price: 48.95},
+            {id: 3, name: "Soccer Ball", category: "Soccer", descriptsion: "FIFA-approved size and weight", price: 19.50},
+            {id: 4, name: "Corner Flags", category: "Soccer", descriptsion: "Give your playing field a professional touch", price: 34.95},
+            {id: 5, name: "Stadium", category: "Soccer", descriptsion: "Flat-packed 35,000-seat stadium", price: 79500},
+            {id: 6, name: "Thinking Cap", category: "Chess", descriptsion: "Improve brain efficiency by 75%", price: 16},
+            {id: 7, name: "Unsteady Chair", category: "Chess", descriptsion: "Secretly give your opponent a disadvantage", price: 29.95},
+            {id: 8, name: "Human Chess Board", category: "Chess", descriptsion: "A fun game for the family", price: 75},
+            {id: 9, name: "Bling Bling King", category: "Chess", descriptsion: "Gold-plated, diamond-studded King", price: 1200},
+        ],
+        orders: []
+    }
+}
